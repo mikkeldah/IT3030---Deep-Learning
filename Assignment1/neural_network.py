Need to fix backprop

Assignment1/neural_network.py
@@ -1,82 +1,47 @@
 import numpy as np
 
-from utils import sigmoid, softmax, add_bias_dimension, remove_bias_dimension
+from utils import *
+from layer import Layer
+
+from enums import * 
+
 
 class NeuralNetwork:
-    """
-    The Network assumes that the first number of the input array is the size of the input layer
-    and the last number of the input array is the size of the output layer. The rest of the 
-    numbers are assumed to be the size of hidden layers in between. 
 
-    The Network uses the same activation function for all of the hidden layers (sigmoid), and
-    Softmax activation for the output layer. 
-
-    """
-
-    def __init__(self, layers_array: np.array, softmax: bool):
-        if not (1 < layers_array.size < 8):
-            raise Exception("Network must contain at least one input and one output layer, and not more than 5 hidden layers. ")
-
-        self.softmax = softmax
+    def __init__(self, cost_function: Cost):
         self.layers = []
-        
-        # Starting at the second layer, as there is no need to create an input layer object as it does not
-        # have weights and biases associated with it
-        for i in range(1, layers_array.size):
-            self.layers.append(Layer(layer_size=layers_array[i], prev_layer_size=layers_array[i-1]))
+        self.cost_function = cost_function
 
-    def forward_pass(self, x, y):
+    def add_layer(self, layer: Layer) -> None: 
+        self.layers.append(layer)
 
-        # Add row of ones to the input matrix to account for the bias trick
-        x = add_bias_dimension(x)
+    def forward_pass(self, x: np.ndarray) -> np.ndarray:   
+        """
+        params
+            x: initial input to the network with shape [input size, batch size]
+
+        return: the final output of the network with shape [batch size, num classes]
+
+        """
 
         for layer in self.layers:
             x = layer.forward(x)
-            x = add_bias_dimension(x)
-        
-        # Remove bias row from the final output
-        x = remove_bias_dimension(x)
 
-        if self.softmax:
-            return softmax(x)
+        return np.transpose(x)
 
-        return x
+    def backward_pass(self, outputs, targets):
 
-    def backward_pass(self):
+        loss = cross_entropy_loss(targets, outputs)
+
+        J_cost_output = -np.sum(targets / outputs, axis=1)
+        print("J_cost_output: ", J_cost_output * np.array([[1, 1], [1, 1]]))
+
+        J_acc = J_cost_output
 
         for layer in reversed(self.layers):
-            layer.backward()
+            J_acc = layer.backward(J_acc) 
             
             
-            
-
-class Layer:
-    def __init__(self, layer_size: int, prev_layer_size: int):
-        # Random init of weights in range [0, 1]. Adding a row to account for the bias trick.
-        self.weights = np.random.rand(prev_layer_size + 1, layer_size)
-
-    def forward(self, x):
-        """
-        Args:
-            x: inputs of shape [previous layer size, batch size]
-        Returns:
-            self.outputs: output of layer with shape [layer size, batch size]
-
-        """
-        self.inputs = x
-        self.outputs = sigmoid(np.transpose(self.weights) @ x)
-
-        return self.outputs 
-    
-    def backward(self):
         
-        
-
-        return
-
-
-    def set_weights(self, w):
-        self.weights = w
-
 
 

Assignment1/test.py
@@ -1,82 +1,35 @@
 import numpy as np
 
+from utils import *
 from neural_network import NeuralNetwork
-from utils import sigmoid, softmax, cross_entropy_loss, add_bias_dimension
-
-# Only for testing
-from keras.datasets import mnist
+from enums import Activation, Cost
+from layer import *
 
 
+X = np.array([[1, 10], [16, 1], [50, 5]])
+y = np.array([[0, 1, 0], [0, 0, 1]])
 
 
-test_data_X = np.array([[1, 1], [2, 1]])
-test_data_y = np.array([1, 2])
+nn = NeuralNetwork(cost_function=Cost.CROSS_ENTROPY_LOSS)
 
-test_data_X2 = np.array([
-    [1, 2, 6, 4], 
-    [2, 3, 1, 7], 
-    [3, 5, 2, 3], 
-    [1, 8, 9, 6]
-])
-
-test_data_y2 = np.array([0, 1, 1, 0])
-
-w1_test = np.array([
-    [1.0, 0.5, 1.0],
-    [0.5, 1.0, 0.5], 
-    [1.0, 1.0, 1.0]
-])
-
-w2_test = np.array([
-    [1.0],
-    [0.5],
-    [1.0], 
-    [1.0]
-])
-
-def test_shapes(nn):
-    hidden_layer = nn.layers[0]
-    output_layer = nn.layers[1]
-    
-    # Testing for correct shapes of weights and biases
-    assert hidden_layer.weights.shape == (3, 3)
-    assert output_layer.weights.shape == (4, 1)
+nn.add_layer(DenseLayer(prev_layer_size=3, layer_size=4))
+nn.add_layer(ActivationLayer(Activation.SIGMOID))
+nn.add_layer(DenseLayer(prev_layer_size=4, layer_size=4))
+nn.add_layer(ActivationLayer(Activation.SIGMOID))
+nn.add_layer(DenseLayer(prev_layer_size=4, layer_size=2))
+nn.add_layer(ActivationLayer(Activation.SIGMOID))
+nn.add_layer(DenseLayer(prev_layer_size=2, layer_size=3))
+nn.add_layer(ActivationLayer(Activation.SIGMOID))
+nn.add_layer(ActivationLayer(Activation.SOFTMAX))
 
 
-# Testing for correct output from forward pass
-def test_forward_pass(nn, w1_test, w2_test, test_data_X):
-    nn.layers[0].set_weights(w1_test)
-    nn.layers[1].set_weights(w2_test)
-    output = nn.forward_pass(test_data_X, test_data_y)
+# Forward pass - output.shape is [batch size, num_classes]
+output = nn.forward_pass(X)
 
-    # Test outside model
-    test_data_X = add_bias_dimension(test_data_X)
-    o1 = sigmoid(np.transpose(w1_test) @ test_data_X )
+# Loss
+loss = cross_entropy_loss(y, output)
+print("Cross Entropy Loss: ", loss)
 
-    o1 = add_bias_dimension(o1)
-    output_true = sigmoid(np.transpose(w2_test) @ o1)    
+# Backward pass
+nn.backward_pass(output, y)
 
-
-    assert output.all() == output_true.all()
-    assert np.isclose([output[0, 0], output[0, 1]], [0.9674060661, 0.9647823295], rtol=1e-6)[0]
-    assert np.isclose([output[0, 0], output[0, 1]], [0.9674060661, 0.9647823295], rtol=1e-6)[1]
-
-    print(output)
-
-
-def network_test_without_softmax():
-    nn = NeuralNetwork(layers_array=np.array([2, 3, 1]), softmax=False)
-    test_shapes(nn)
-    test_forward_pass(nn, w1_test, w2_test, test_data_X)
-
-def network_test_with_softmax():
-    nn2 = NeuralNetwork(layers_array=np.array([test_data_X2.shape[0], 6, 4, 3]), softmax=True)
-    o = nn2.forward_pass(test_data_X2, test_data_y2)
-    print(o)
-
-if __name__ == "__main__":
-    network_test_without_softmax()
-    network_test_with_softmax()
-
-
-   

Assignment1/utils.py
@@ -4,13 +4,12 @@ def sigmoid(z: np.ndarray) -> np.ndarray:
     return 1 / (1 + np.exp(-z))
 
 def softmax(output: np.ndarray) -> np.ndarray:
-    return np.exp(output) / np.exp(output).sum()
+    print(np.exp(output))
+    print(sum(np.exp(output)))
+    return np.exp(output) / np.exp(output).sum(axis=0)
 
 def cross_entropy_loss(targets: np.ndarray, outputs: np.ndarray) -> float:
-
-    loss_vector = -(targets * np.log(outputs) + (1 - targets) * np.log(1 - outputs))
-    cross_entropy_loss = (1 / targets.shape[0]) * np.sum(loss_vector)
-
+    cross_entropy_loss = np.mean(-np.sum(targets * np.log(outputs), axis=1))
     return cross_entropy_loss
 
 def add_bias_dimension(x: np.array):
@@ -19,4 +18,11 @@ def add_bias_dimension(x: np.array):
 
 def remove_bias_dimension(x: np.array):
     return x[:-1]
+
+def one_hot_encode(y: np.ndarray, n_classes: int):
+    y_encoded = np.zeros((y.shape[0], n_classes))
+    y_encoded[np.arange(y.shape[0]), y.flatten()] = 1
+
+    return y_encoded
+
 