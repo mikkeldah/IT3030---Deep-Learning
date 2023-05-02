e1c68c724213504f3d9230b35599fbf2a6887124
Mikkel Dahl Slettebø <mikkelds@clab16.idi.ntnu.no>
2023-05-02 12:05:58 +0200

    .

--- Project 3 - Deep Generative Models/models/auto_encoder.py
+++ Project 3 - Deep Generative Models/models/auto_encoder.py
@@ -5,32 +5,29 @@ class AutoEncoder(nn.Module):
     def __init__(self, in_channels):
         super(AutoEncoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels, 16, kernel_size=3, stride=2, padding=1), # Output size: [16, 28, 28]
+            nn.Conv2d(in_channels, 16, kernel_size=3, stride=2, padding=1), # Output size: [16, 14, 14]
             nn.ReLU(),
-            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1), # Output size: [32, 14, 14]
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1), # Output size: [32, 7, 7]
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1), # Output size: [64, 7, 7]
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1), # Output size: [64, 4, 4]
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1), # Output size: [128, 4, 4]
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1), # Output size: [128, 2, 2]
             nn.ReLU(),
-            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1), # Output size: [256, 2, 2]
-            nn.ReLU(),
-            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1), # Output size: [512, 1, 1]
-            nn.ReLU()
+            nn.Flatten(),
+            nn.Linear(128*2*2, 64), 
         )
-        
+
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1), 
+            nn.Linear(64, 128*2*2),
             nn.ReLU(),
-            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1), 
+            nn.Unflatten(1, (128, 2 , 2)),
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1), 
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1), 
-            nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1), 
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1), 
             nn.ReLU(),
             nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1), 
             nn.ReLU(),
-            nn.ConvTranspose2d(16, in_channels, kernel_size=3, stride=1, padding=1), 
+            nn.ConvTranspose2d(16, in_channels, kernel_size=3, stride=2, padding=1, output_padding=1), 
             nn.Sigmoid()
         )
         
